6bcc27297bcf231cfc7a0a0878e14429ac734cf5
Oleks <oleksandrbrittner@gmail.com>
2024-06-09 12:42:19 +0200

    asd

--- cube3d.h
+++ cube3d.h
@@ -6,13 +6,18 @@
 /*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:32:29 by obrittne          #+#    #+#             */
-/*   Updated: 2024/06/07 11:25:39 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/09 12:19:50 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE3D_H
 # define CUBE3D_H
 
+# include <math.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
 # include "MLX42/include/MLX42/MLX42.h"
 
 typedef struct s_player
@@ -37,12 +42,10 @@ typedef struct s_data
 	uint32_t	floor;
 }	t_data;
 
-typedef	struct s_cube
-{
-	double	points[8][3];
-	int	amount;
-}	t_cube;
+# define WIDTH 512
+# define HEIGHT 512
 
-
+int		display(t_data *data);
+void	move(void *param);
 
 #endif
