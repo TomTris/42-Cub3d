724c3b89c83ad3a8a672d5d0ab1fa4227372e097
TomTris <quoctrido.jb@gmail.com>
2024-06-09 20:06:01 +0200

    put libft to parsing, add soe func

--- parsing/ft_file_create.c
+++ parsing/ft_file_create.c
@@ -6,7 +6,7 @@
 /*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/09 17:00:08 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/09 18:05:21 by qdo              ###   ########.fr       */
+/*   Updated: 2024/06/09 19:48:42 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	ft_check_args(int ac, char **av)
 	if (len <= 3 || av[1][len - 1] != 'b' || av[1][len -2] != 'u'
 		|| av[1][len - 3] != 'b' || av[1][len - 4] != '.')
 	{
-		printf("wrong name\n");
+		print_fd(2, "Error\nToo little arguments\n");
 		return (0);
 	}
 	return (1);
@@ -38,13 +38,9 @@ char	**ft_file_create3(char **ret)
 	i = 0;
 	while (ret[i])
 		i++;
-	i -= 1;
-	while (is_empty_line(ret[i]) == 1)
-	{
-		free(ret[i]);
-		ret[i] = 0;
-		i--;
-	}
+	if (i < 9)
+		return (print_fd(2, "Error\nToo little lines\n"),
+			free_split(ret), NULL);
 }
 
 char	**ft_file_create2(char **ret, int fd)
@@ -65,12 +61,9 @@ char	**ft_file_create2(char **ret, int fd)
 		ret = smerge(ret, temp);
 		free(temp);
 		if (ret == 0)
-			return (perror("malloc"), close(fd), NULL);
+			return (close(fd), NULL);
 		temp = get_next_line(fd);
 	}
-	if (ret[0] == NULL)
-		return (print_fd(2, "invalid map"),
-			close(fd), free_split(ret), NULL);
 	if (close(fd) == -1)
 		return (perror("close"), free_split(ret), NULL);
 	return (ft_file_create3(ret));
@@ -86,7 +79,7 @@ char	**ft_file_create(int ac, char **av, char *file_name)
 		return (NULL);
 	fd = open(file_name, O_RDONLY);
 	if (fd < 0)
-		return (perror("open"), 0);
+		return (perror("Error\nOpen"), 0);
 	ret = smerge(0, 0);
 	if (ret == 0)
 		return (NULL);
