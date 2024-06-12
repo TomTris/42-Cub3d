e8d4d58be2ee57ece3f4e7381afb73d6b3d25f8e
Oleks <oleksandrbrittner@gmail.com>
2024-06-12 20:38:48 +0200

    asd

--- rays/get_cords.c
+++ rays/get_cords.c
@@ -6,7 +6,7 @@
 /*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 18:36:45 by obrittne          #+#    #+#             */
-/*   Updated: 2024/06/12 19:38:19 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/12 19:41:49 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ void	get_horizontal(t_data *data, double angle, t_ray *ray)
 	else
 	{
 		y_factor = -1.0;
-		ray->t_y = (double)((int)data->player->y);
+		ray->t_y = (double)((int)data->player->y) - 0.000001;
 		ray->t_x = data->player->x + fabs(((double)(ray->t_y - data->player->y))) * ta * x_factor;
 	}
 	if ((int)ray->t_x < 0 || (int)ray->t_x >= data->map_width || data->map[(int)ray->t_y][(int)ray->t_x] == '1')
@@ -140,41 +140,27 @@ void	get_vertical(t_data *data, double angle, t_ray *ray)
 	ray->y = ray->t_yv;
 }
 
-// void	get_best(t_data *data, t_ray *ray)
-// {
-// 	// if (pow(ray->t_xv - data->player->x, 2.0) + pow(ray->t_yv - data->player->y, 2.0) > \
-// 	// pow(ray->t_x - data->player->x, 2.0) + pow(ray->t_y - data->player->y, 2.0))
-// 	// {
-// 	// 	ray->x = ray->t_x;
-// 	// 	ray->y = ray->t_y;
-// 	// 	if (ray->should)
-// 	// 	{
-// 	// 		ray->x += ray->update_x;
-// 	// 		ray->y += ray->update_y;
-// 	// 	}
-// 	// }
-// 	// else
-// 	// {
-// 	// 	ray->x = ray->t_xv;
-// 	// 	ray->y = ray->t_yv;
-// 	// 	if (ray->shouldv)
-// 	// 	{
-// 	// 		ray->x += ray->update_xv;
-// 	// 		ray->y += ray->update_yv;
-// 	// 	}
-// 	// }
-// 	data  += 0;
-
-// }
+void	get_best(t_data *data, t_ray *ray)
+{
+	if (pow(ray->t_xv - data->player->x, 2.0) + pow(ray->t_yv - data->player->y, 2.0) > \
+	pow(ray->t_x - data->player->x, 2.0) + pow(ray->t_y - data->player->y, 2.0))
+	{
+		ray->x = ray->t_x;
+		ray->y = ray->t_y;
+	}
+	else
+	{
+		ray->x = ray->t_xv;
+		ray->y = ray->t_yv;
+	}
+}
 
 void	get_normal(t_data *data, double angle, t_ray *ray)
 {
-	// get_horizontal(data, angle, ray);
+	get_horizontal(data, angle, ray);
 	get_vertical(data, angle, ray);
 	dprintf(1, "END\n");
-	// get_best(data, ray);
-		ray->x = ray->t_xv;
-	ray->y = ray->t_yv;
+	get_best(data, ray);
 }
 
 void	get_cords(t_data *data, double angle, t_ray *ray)
