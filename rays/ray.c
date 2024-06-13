f2dd022e0893b675722ea773439b3fa31af4f797
Oleks <oleksandrbrittner@gmail.com>
2024-06-13 16:21:41 +0200

    asd

--- rays/ray.c
+++ rays/ray.c
@@ -6,7 +6,7 @@
 /*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 18:17:44 by obrittne          #+#    #+#             */
-/*   Updated: 2024/06/12 20:31:29 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/13 15:12:14 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ void	draw_rays(t_data *data)
 	{
 	get_cords(data, add_angles(data->player->angle_turn_horizontal, M_PI / 6000.0 * ((double) i) - (M_PI / 6.0) ), &ray[i]);
 	transform_cordinates(data, &ray[i]);
-	dprintf(1, "x_cord {%f} y_cord{%f}", ray->x, ray->y);
 	DDA(data, &ray[i]);
 	}
 }
