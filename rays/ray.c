c6a6efe79a8611c7d244e65e7a28b70cec1629d4
Oleks <oleksandrbrittner@gmail.com>
2024-06-13 20:30:15 +0200

    a

--- rays/ray.c
+++ rays/ray.c
@@ -6,7 +6,7 @@
 /*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 18:17:44 by obrittne          #+#    #+#             */
-/*   Updated: 2024/06/13 19:44:29 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/13 20:16:54 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,47 @@ double *distance1, double *distance2)
 	pow(ray->t_y - data->player->y, 2.0));
 }
 
+mlx_texture_t	*get_texture(t_data *data, int texture)
+{
+	if (texture == 0)
+		return (data->ea);
+	else if (texture == 1)
+		return (data->no);
+	else if (texture == 2)
+		return (data->we);
+	else
+		return (data->so);
+}
+
+int	get_wid_on_texture(t_data *data, t_ray *ray)
+{
+	mlx_texture_t	*temp;
+	double			t;
+
+	temp = get_texture(data, ray->texture);
+	if (ray->texture == 0)
+	{
+		t = fabs(ray->x - (double)((int)ray->x));
+	}
+	else if (ray->texture == 1)
+	{
+		t = fabs(ray->y - (double)((int)ray->y));
+	}
+	else if (ray->texture == 2)
+	{
+		t = fabs(ray->x - (double)((int)ray->x));
+	}
+	else
+	{
+		t = fabs(ray->y - (double)((int)ray->y));
+	}
+	return ((int)lround(t * temp->width));
+}
+
 void	draw_rays(t_data *data, t_ray *ray)
 {
-	int	i;
+	int				i;
+	mlx_texture_t	*temp;
 
 	i = 0;
 	while (i < AMOUNT_OF_RAYS)
@@ -32,6 +70,9 @@ void	draw_rays(t_data *data, t_ray *ray)
 		M_PI / 3.0 / AMOUNT_OF_RAYS * ((double) i) - (M_PI / 6.0)), &ray[i]);
 		transform_cordinates(data, &ray[i]);
 		DDA(data, &ray[i]);
+
+		temp = get_texture(data, ray->texture);
+		render_height(data->image, temp, temp->height, 1.0 / ray[i].distance, get_wid_on_texture(data, ray), i);
 		i++;
 	}
 }
