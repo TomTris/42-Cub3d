b73b166c0dbf79e597f835948f9b724ab4208ec7
Oleks <oleksandrbrittner@gmail.com>
2024-06-13 21:17:23 +0200

    ads

--- To/get_pixel.c
+++ To/get_pixel.c
@@ -6,30 +6,33 @@
 /*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 18:35:48 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/13 20:54:31 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/13 21:12:06 by obrittne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cube3d.h"
 
-#define WINDOW_HEIGHT 1024
-#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 1023
+#define WINDOW_WIDTH 1023
 
 unsigned int	get_pixel(mlx_texture_t *texture, int wid, int hei)
 {
 	int		ret;
 	uint8_t	*temp;
 	int		i;
-
+printf("3\n");
 	temp = &texture->pixels[(wid * texture->width + hei) * sizeof(u_int32_t)];
+	// printf("4\n");
 	ret = 0;
 	i = -1;
+	// printf("6\n");
 	while (++i < 4)
 	{
 		ret = ret << 8;
 		ret = ret | *temp;
 		temp += 1;
 	}
+	// printf("5\n");
 	return (ret);
 }
 
@@ -47,27 +50,37 @@ unsigned int	get_pixel(mlx_texture_t *texture, int wid, int hei)
 
 void	render_height(mlx_image_t *image, mlx_texture_t *texture, int texture_hei, double rate, int wid_on_texture, int wid_on_screen)
 {
+	// return ;
+	// printf("5\n");
 	int	hei_highest = WINDOW_HEIGHT / 2 + WINDOW_HEIGHT * rate;
+	// printf("5\n");
 	int	height_cnt = WINDOW_HEIGHT;
 	int height_on_texture;
-	int	a = 0;
 
-	while (height_cnt > hei_highest)
+	if (rate <= 1)
 	{
-		mlx_put_pixel(image, wid_on_screen, height_cnt--, 100<<16 | 255);
-		// printf("a\n");
-	}
-	hei_highest = WINDOW_HEIGHT / 2 - rate * WINDOW_HEIGHT;
-	while (height_cnt > hei_highest)
-	{
-		height_on_texture = (height_cnt - hei_highest) / (WINDOW_HEIGHT - 2 * hei_highest) * texture_hei;
-		mlx_put_pixel(image, wid_on_screen, height_cnt, get_pixel(texture, wid_on_texture, height_on_texture));
-		height_cnt--;
-	}
-	while (height_cnt > 0)
-	{
-		mlx_put_pixel(image, wid_on_screen, height_cnt--, 255 << 8 | 10);
-		// printf("a =\n");
+		while (height_cnt > hei_highest)
+		{
+			mlx_put_pixel(image, wid_on_screen, height_cnt--, 100<<16 | 255);
+			// printf("a\n");
+		}
+		// printf("6\n");
+		hei_highest = (WINDOW_HEIGHT - rate * WINDOW_HEIGHT) / 2;
+		// printf("7\n");
+		while (height_cnt > hei_highest)
+		{
+			// printf("8\n");
+			height_on_texture = (height_cnt - hei_highest) / (WINDOW_HEIGHT - 2 * hei_highest) * texture_hei;
+			printf("9\n");
+			printf("1 = {%d}, 2 = {%d}\n", wid_on_screen, height_cnt);
+			mlx_put_pixel(image, wid_on_screen, height_cnt, get_pixel(texture, wid_on_texture, height_on_texture));
+			height_cnt--;
+		}
+		while (height_cnt > 0)
+		{
+			mlx_put_pixel(image, wid_on_screen, height_cnt--, 255 << 8 | 10);
+			// printf("a =\n");
+		}
 	}
 }
 
