3a751a7b27036e61ff0178c79f27f7e13bb24839
TomTris <quoctrido.jb@gmail.com>
2024-06-14 18:35:10 +0200

    get pixel updated

--- To/get_pixel.c
+++ To/get_pixel.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_pixel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obrittne <obrittne@student.42.fr>          +#+  +:+       +#+        */
+/*   By: qdo <qdo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 18:35:48 by qdo               #+#    #+#             */
-/*   Updated: 2024/06/14 10:57:27 by obrittne         ###   ########.fr       */
+/*   Updated: 2024/06/14 18:29:38 by qdo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,12 @@
 
 #define WINDOW_HEIGHT 1023
 #define WINDOW_WIDTH 1023
-#define RATE_SCREEN 1/2
 
 int	raya(int a1, int b)
 {
-	static int a = 0;
+	static int	a = 0;
+
+	a = 0;
 
 	if (b == 1)
 		return (a);
@@ -38,50 +39,67 @@ unsigned int	get_pixel(mlx_texture_t *texture, int wid, int hei)
 	while (++i < 4)
 	{
 		ret = ret << 8;
-		// if (temp == 0)
-		// 	ret = ret | 0;
-		// else
-		// {
-			// write(1, "a\n", 2);
+		if (temp == 0)
+			ret = ret | 0;
+		else
 			ret = ret | temp[i];
-			// write(1, "b\n", 2);
-		// }
 	}
 	return (ret);
 }
 
-
-void	render_height(mlx_image_t *image, mlx_texture_t *texture, int texture_hei, double rate, int wid_on_texture, int wid_on_screen)
+void	render_height_less_than_1(mlx_image_t *image, mlx_texture_t *texture, int texture_hei, double rate, int wid_on_texture, int wid_on_screen)
 {
 	double	height_on_texture;
 	double	height_cnt ;
 	double	hei_highest ;
 	int		a;
 	int		b;
-	if (rate >= 1)
-	{
-		height_cnt = WINDOW_HEIGHT;
-		hei_highest = 0;
-	}
-	else
-	{
-		height_cnt = (WINDOW_HEIGHT  + WINDOW_HEIGHT * rate) / 2;
-		hei_highest = (WINDOW_HEIGHT  - WINDOW_HEIGHT * rate) / 2;
-	}
+	
+	height_cnt = (WINDOW_HEIGHT  + WINDOW_HEIGHT * rate) / 2;
+	hei_highest = (WINDOW_HEIGHT  - WINDOW_HEIGHT * rate) / 2;
 	while (height_cnt >= hei_highest)
 	{
 		height_on_texture =  (height_cnt - hei_highest) /  (WINDOW_HEIGHT - 2 * hei_highest) *  texture_hei;
+		a = (int) lround(height_on_texture);
 		if ((int) lround(height_on_texture) == texture_hei)
 			a = (int) lround(height_on_texture) - 1;
-		else
-			a = (int) lround(height_on_texture);
+		b = wid_on_texture;
 		if (wid_on_texture > 0)
 			b = wid_on_texture - 1;
-		else
-			b = wid_on_texture;
 		mlx_put_pixel(image, wid_on_screen,(int) height_cnt, get_pixel(texture, b, a));
 		height_cnt -= 1;
 	}
+}
+
+void	render_height_more_than_1(mlx_image_t *image, mlx_texture_t *texture, int texture_hei, double rate, int wid_on_texture, int wid_on_screen)
+{
+	double	height_on_texture;
+	double	height_cnt = WINDOW_HEIGHT;
+	int		a;
+	int		b;
+	double down_part = ((rate - 1) / 2) / rate * texture_hei;
+
+	while (height_cnt >= 0)
+	{
+		height_on_texture =  (height_cnt /  WINDOW_HEIGHT) * texture_hei / rate + down_part;
+		a = (int) lround(height_on_texture);
+		if ((int) lround(height_on_texture) == texture_hei)
+			a = (int) lround(height_on_texture) - 1;
+		b = wid_on_texture;
+		if (wid_on_texture > 0)
+			b = wid_on_texture - 1;
+		mlx_put_pixel(image, wid_on_screen,(int) height_cnt, get_pixel(texture, b, a));
+		height_cnt -= 1;
+	}
+}
+
+
+void	render_height(mlx_image_t *image, mlx_texture_t *texture, int texture_hei, double rate, int wid_on_texture, int wid_on_screen)
+{
+	if (rate > 1)
+		render_height_more_than_1(image, texture, texture_hei, rate, wid_on_texture, wid_on_screen);
+	else
+		render_height_less_than_1(image, texture, texture_hei, rate, wid_on_texture, wid_on_screen);
 }
 
 // mlx_put_pixel(data.image, i, j, 255 << 16 | 10);
